df3f6ffcc9e86204459fa507ad06d23f84411782
Christopher <christiller2015@gmail.com>
2021-02-16 19:17:15 -0500

    Wire up hidden input on form field to send back series id

--- dhf_website/static/js/script.js
+++ dhf_website/static/js/script.js
@@ -3,6 +3,7 @@
 const webPrefix = window.location.href.split('/').slice(0, 3).join('/');
 
 const characterSeriesInput = document.querySelector('#id_character_series');
+const characterSeriesIdInput = document.querySelector('#id_character_series_id');
 
 const totalForms = document.querySelector("#id_relations-form-TOTAL_FORMS");
 const totalReferenceForms = document.querySelector("#id_references-form-TOTAL_FORMS");
@@ -17,6 +18,24 @@ const characterForm = document.querySelector("#new_character_form");
 let relationsFormCount = relationForm.length - 1;
 let referencesFormCount = referenceForm.length - 1;
 
+const createAutocompleteRow = (formField, formIdField, text, id) => {
+	const newDiv = document.createElement("div");
+
+	const textContent = document.createTextNode(text);
+
+	newDiv.appendChild(textContent);
+	console.log('Adding event listener!');
+	newDiv.addEventListener('click', (event) => {
+		formField.value = text;
+		formIdField.value = id;
+		console.log(formField);
+		console.log('Clicked!', id);
+	});
+	console.log(newDiv);
+
+	return newDiv;
+}
+
 let seriesAutocompleteSuggest = [];
 
 function debounce (callback, wait) {
@@ -28,6 +47,7 @@ function debounce (callback, wait) {
 }
 
 const handleKeypress = debounce(async (str) => {
+
 	const autocompleteRequest = `${webPrefix}/autocomplete/series?search_text=${str}`;
 	
 	const response = await fetch(autocompleteRequest, {
@@ -40,8 +60,22 @@ const handleKeypress = debounce(async (str) => {
 		return result.json();
 	})
 
-	seriesAutocompleteSuggest = [...response.series];
-	console.log(seriesAutocompleteSuggest);
+	// doing too many things. Break out into separate functions.
+	seriesAutocompleteSuggest = [...response.series].map(m => createAutocompleteRow(characterSeriesInput, characterSeriesIdInput, m.name, m.id));
+	const autocompleteList = document.createElement('div');
+
+	seriesAutocompleteSuggest.forEach((item) => {
+		autocompleteList.appendChild(item);	
+	});
+
+	// instead of clobbering the div entirely, just replace the content.
+	if(characterSeriesInput.nextSibling) {
+		characterSeriesInput.nextSibling.innerHTML = '';
+		characterSeriesInput.parentNode.insertBefore(autocompleteList, characterSeriesInput.nextSibling);
+	} else {
+		characterSeriesInput.parentNode.insertBefore(autocompleteList, characterSeriesInput.nextSibling);
+	}
+
 }, 500);
 
 characterSeriesInput.addEventListener("input", (event) => { 
